9564331e3bd2cd2a5f728637621d4731ff67c1d7
Mrithika S <saravananmrithika08@gmail.com>
2026-06-04 19:17:47 +0530

    Add first Python script

---
+++ hello.py
@@ -0,0 +1,10 @@
+# My first python script on Github
+# Mrithika - Biomedical Engineering Student
+
+name = "Mrithika"
+field = "Biomedical Engineering"
+college = "Easwari Engineering College"
+
+print(f"Hello! My name is {name}.")
+print(f"I am studying {field} at {college}.")
+print("This is my first Github commit!")
